c0b0e9fb82486d714d24fe4a32ab1760bf423044
hiroka1642 <uver7770422@gmail.com>
2022-08-15 02:24:41 +0900

    🩹 Fix  Title

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -7,7 +7,7 @@ type Props = {
 export const Title: FC<Props> = (props) => {
   const { children } = props;
   return (
-    <div className="font-bold text-3xl border-collapse border-b-2 border-l-slate-700">
+    <div className="font-bold text-3xl border-b-2">
       <h2 className="py-5">{children}</h2>
     </div>
   );
