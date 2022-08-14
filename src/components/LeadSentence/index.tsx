56989d6d50250d65600316917987243f2b4de6ff
hiroka1642 <uver7770422@gmail.com>
2022-08-15 02:06:50 +0900

    🚚 ディレクトリ移動

--- src/components/LeadSentence/index.tsx
+++ src/components/LeadSentence/index.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
-import { FacebookIcon } from "../Icons/Facebook";
-import { RssIcon } from "../Icons/Rss";
-import { TwitterIcon } from "../Icons/Twitter";
+import { FacebookIcon } from "../Icons/facebook";
+import { RssIcon } from "../Icons/rss";
+import { TwitterIcon } from "../Icons/twitter";
 import { Layout } from "../Layout";
 
 export const LeadSentence: FC = () => {
@@ -11,7 +11,7 @@ export const LeadSentence: FC = () => {
         <Layout>
           <div className="flex justify-between text-white font-bold h-64 items-center">
             <div>
-              <h1>Shimabu&nbsp;IT&nbsp;University</h1>
+              <h1 className="text-4xl py-2">Shimabu&nbsp;IT&nbsp;University</h1>
               <p>しまぶーのポートフォリオのためのページです</p>
             </div>
             <div className="flex gap-2 justify-between ">
