9e17378f3d0974be7a690cc552a1a2eec4bf9a51
hiroka1642 <uver7770422@gmail.com>
2022-08-15 02:24:13 +0900

    ✨ Footer作成

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import { Layout } from "../Layout";
+
+export const Footer: FC = () => {
+  return (
+    <div className="border-t-2">
+      <Layout>
+        <div className="bg-white text-slate-600 w-full h-16 flex items-center justify-center">
+          <p>© 2022 Shimabu IT University</p>
+        </div>
+      </Layout>
+    </div>
+  );
+};
