f6d6ba41fca8c2277489cb722da31824f659b7c1
hiroka1642 <uver7770422@gmail.com>
2022-08-15 22:57:34 +0900

    ✨ Blogページ作成

---
+++ pages/Blog.tsx
@@ -0,0 +1,59 @@
+import { Footer } from "../src/components/Footer";
+import { Header } from "../src/components/Header";
+import { Title } from "../src/components/Title";
+
+const Blog = () => {
+  return (
+    <>
+      <Header />
+      <div className="mx-56 mt-10 mb-20">
+        <Title>Blog</Title>
+        <h4 className="text-2xl font-semibold mb-2 mt-6">This is a header</h4>
+        <p className="leading-7 text-lg tracking-wide">
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+          sint. Velit officia consequat duis enim velit mollit.
+        </p>
+        <p className="leading-7">2022.07.11</p>
+        <h4 className="text-2xl font-semibold mb-2 mt-6">This is a header</h4>
+        <p className="leading-7 text-lg tracking-wide">
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+          sint. Velit officia consequat duis enim velit mollit.
+        </p>
+        <p className="leading-7">2022.07.11</p>
+        <h4 className="text-2xl font-semibold mb-2 mt-6">This is a header</h4>
+        <p className="leading-7 text-lg tracking-wide">
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+          sint. Velit officia consequat duis enim velit mollit.
+        </p>
+        <p className="leading-7">2022.07.11</p>
+        <h4 className="text-2xl font-semibold mb-2 mt-6">This is a header</h4>
+        <p className="leading-7 text-lg tracking-wide">
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+          sint. Velit officia consequat duis enim velit mollit.
+        </p>
+        <p className="leading-7">2022.07.11</p>
+        <h4 className="text-2xl font-semibold mb-2 mt-6">This is a header</h4>
+        <p className="leading-7 text-lg tracking-wide">
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+          sint. Velit officia consequat duis enim velit mollit.
+        </p>
+        <p className="leading-7">2022.07.11</p>
+        <h4 className="text-2xl font-semibold mb-2 mt-6">This is a header</h4>
+        <p className="leading-7 text-lg tracking-wide">
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+          sint. Velit officia consequat duis enim velit mollit.
+        </p>
+        <p className="leading-7">2022.07.11</p>
+        <h4 className="text-2xl font-semibold mb-2 mt-6">This is a header</h4>
+        <p className="leading-7 text-lg tracking-wide">
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+          sint. Velit officia consequat duis enim velit mollit.
+        </p>
+        <p className="leading-7">2022.07.11</p>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Blog;
