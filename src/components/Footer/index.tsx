603bda62692465186ce993fb156a26629bb9e4ba
hiroka1642 <uver7770422@gmail.com>
2022-08-15 23:08:22 +0900

    🩹 fix

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import { Layout } from "../Layout";
 
 export const Footer: FC = () => {
   return (
-    <div className="border-t-2">
+    <div className="border-t-2 w-full">
       <Layout>
         <div className="bg-white text-slate-600 w-full h-16 flex items-center justify-center">
           <p>© 2022 Shimabu IT University</p>
