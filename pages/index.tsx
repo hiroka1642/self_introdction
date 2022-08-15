d0120a15d298be0dc1062aa2ccff699487422638
hiroka1642 <uver7770422@gmail.com>
2022-08-15 22:29:00 +0900

    ✨ トップページ　レイアウト作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,7 @@
+import { Container } from "../src/components/Container";
+import { Footer } from "../src/components/Footer";
 import { Header } from "../src/components/Header";
+import { Layout } from "../src/components/Layout";
 import { LeadSentence } from "../src/components/LeadSentence";
 
 const Home = () => {
@@ -6,6 +9,27 @@ const Home = () => {
     <>
       <Header />
       <LeadSentence />
+      <Layout>
+        <Container title="Blog" button="View All">
+          テキストテキストテキストテキストテキストテキストテキスト
+        </Container>
+      </Layout>
+      <Layout>
+        <Container title="Portfolio" button="View All">
+          テキストテキストテキストテキストテキストテキストテキスト
+        </Container>
+      </Layout>
+      <Layout>
+        <div className="flex flex-row gap-16 mb-20">
+          <Container title="GitHub" button="View on GitHub">
+            テキストテキストテキストテキストテキストテキストテキスト
+          </Container>
+          <Container title="Twitter" button="View on Twitter">
+            テキストテキストテキストテキストテキストテキストテキスト
+          </Container>
+        </div>
+      </Layout>
+      <Footer />
     </>
   );
 };
